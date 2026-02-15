9db04b18c9982062e5482298d25705de5520e598
SttackSite <sttacksite@gmail.com>
2026-02-15 12:21:44 -0300

    Update landing.py

--- landing.py
+++ landing.py
@@ -439,7 +439,7 @@ st.markdown("""
 <div class="carousel-section">
     <div class="carousel-container">
         <a href="https://www.google.com/" target="_blank" class="carousel-item-link">
-            <img src="https://raw.githubusercontent.com/Gm0ur4/cortex-demo/main/20.png" alt="Template 1">
+            <img src="https://raw.githubusercontent.com/SttackSite/site/main/testeimagem1.png" alt="Template 1">
         </a>
         <a href="https://www.google.com/" target="_blank" class="carousel-item-link">
             <img src="https://raw.githubusercontent.com/Gm0ur4/cortex-demo/main/17.png" alt="Template 2">
