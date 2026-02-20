b05a044d1e12260d0c38d5f7a0b9a7fc2b7cad34
SttackSite <sttacksite@gmail.com>
2026-02-20 09:39:06 -0300

    Update landing.py

--- landing.py
+++ landing.py
@@ -472,8 +472,8 @@ st.markdown('<h2>PROCESSO <span class="serif-heavy">sem falhas.</span></h2><br><
 
 steps = [
     ("SELECIONE O MODELO IDEAL", "Escolha entre mais de 30 modelos validados o que mais combina com a identidade do seu negócio."),
-    ("CUSTOMIZAÇÃO RÁPIDA", "Utilize nosso passo a passo detalhado para implementar o código e personalizar cada detalhe sem complicações."),
-    ("SETUP TÉCNICO GRATUITO", "Te ensinamos onde hospedar seu site em segundos, como aplicar técnicas de SEO e configurar seu domínio personalizado sem custo adicional e de forma rápida."),
+    ("CUSTOMIZAÇÃO RÁPIDA", "Utilize nosso passo a passo detalhado para personalizar cada detalhe sem complicações."),
+    ("SETUP TÉCNICO GRATUITO", "Hospedamos seu site em minutos, como as melhores técnicas de SEO e configuraros seu domínio personalizado sem custo adicional e de forma rápida."),
     ("LANÇAMENTO IMEDIATO", "Site no ar, otimizado e pronto para escalar seu negócio com uma estrutura de alta performance.")
 ]
 
