7a5adae6389136b6cb4150119cfdeeef5d433bbe
SttackSite <sttacksite@gmail.com>
2026-02-27 15:25:26 -0300

    Update landing.py

--- landing.py
+++ landing.py
@@ -199,7 +199,7 @@ st.markdown("""
     .carousel-item-image-only {
         flex: 0 0 800px;
         min-width: 800px;
-        height: 900px;
+        height: 800px;
         border-radius: 8px;
         overflow-y: auto;
         overflow-x: hidden;
