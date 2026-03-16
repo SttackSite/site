0f0a52b8ec2f9d69c1fbd90afa835416da9341a1
SttackSite <sttacksite@gmail.com>
2026-03-16 14:15:36 -0300

    Update landing.py

--- landing.py
+++ landing.py
@@ -495,7 +495,7 @@ st.markdown('</div>', unsafe_allow_html=True)
 
 # --- 3 & 4. SHOWCASE DE TEMPLATES (GRID 2D COM SCROLL) ---
 st.markdown('<div id="templates" style="padding: 120px 8%;">', unsafe_allow_html=True)
-st.markdown('<h2>Clique e explore por completo os templates ideais para <span class="serif-heavy"> seu negócio:</span></h2><br><br>', unsafe_allow_html=True)
+st.markdown('<h2>Navegue lateralmente e clique para conhecer o template ideal para <span class="serif-heavy"> seu negócio:</span></h2><br><br>', unsafe_allow_html=True)
 st.markdown("""
 <div class="carousel-section" style="padding: 0; background: transparent;">
     <div class="carousel-container">
